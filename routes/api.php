a75ca0893ee4655030b816e0ef9cb2bbb9150472
Lorenzo Rojo <lorenzorojo12@gmail.com>
2022-04-03 18:04:07 -0500

    Carga del proyecto - Se carga la configuracion realizada para la api v1. - Se configuran rutas web para poder ver un resumen de todos los Post - Se configurarn las rutas api para tener el acceso api (ver todos los post, ver un post en especifico y eliminar un post)

---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('v1/posts',App\Http\Controllers\Api\V1\PostController::class)
+    ->only(['index','show','destroy']);
