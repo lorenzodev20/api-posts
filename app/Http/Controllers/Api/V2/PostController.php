a7f1503f5585905f002a8afa0b5b98bc55bbdf39
Lorenzo Rojo <lorenzorojo12@gmail.com>
2022-04-10 17:04:13 -0500

    Ajustes de V1, V2 y Login Api

---
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+use App\Http\Resources\V2\PostResource;
+use App\Http\Resources\V2\PostCollection;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return new PostCollection(Post::latest()->paginate());
+    }
+
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+
+}
